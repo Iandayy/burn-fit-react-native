34c2e011ee1328098e553a4ba225e83d975a8e9d
ian <onlordev@gmail.com>
2022-12-06 14:21:37 +0900

    ref dates

--- myapp/src/page/calendar/Dates.jsx
+++ myapp/src/page/calendar/Dates.jsx
@@ -1,11 +1,15 @@
 import { StyleSheet, View, ActivityIndicator } from "react-native";
 import { useState, useEffect } from "react";
 
+import shortid from "shortid";
+
 import DateItem from "./common/DateItem";
 
 const Dates = ({ day, today }) => {
   const [allDate, setAllDate] = useState({});
-  const [currentDate, setCurrentDate] = useState(today.getDate());
+  const [currentDate, setCurrentDate] = useState(
+    `${day.year}-${day.month}-${today.getDate()}`
+  );
 
   useEffect(() => {
     const firstDay = parseInt(new Date(day.year, day.month - 1, 2).getDay());
@@ -39,43 +43,16 @@ const Dates = ({ day, today }) => {
       {allDate.length === 0 && <ActivityIndicator />}
       {allDate.length > 0 && (
         <View style={styles.numDays}>
-          <DateItem
-            day={day}
-            allDate={allDate[0]}
-            currentDate={currentDate}
-            setCurrentDate={setCurrentDate}
-          />
-          <DateItem
-            day={day}
-            today={today}
-            allDate={allDate[1]}
-            currentDate={currentDate}
-            setCurrentDate={setCurrentDate}
-          />
-          <DateItem
-            day={day}
-            allDate={allDate[2]}
-            currentDate={currentDate}
-            setCurrentDate={setCurrentDate}
-          />
-          <DateItem
-            day={day}
-            allDate={allDate[3]}
-            currentDate={currentDate}
-            setCurrentDate={setCurrentDate}
-          />
-          <DateItem
-            day={day}
-            allDate={allDate[4]}
-            currentDate={currentDate}
-            setCurrentDate={setCurrentDate}
-          />
-          <DateItem
-            day={day}
-            allDate={allDate[5]}
-            currentDate={currentDate}
-            setCurrentDate={setCurrentDate}
-          />
+          {allDate.map((dates) => (
+            <DateItem
+              key={shortid.generate()}
+              day={day}
+              today={today}
+              dates={dates}
+              currentDate={currentDate}
+              setCurrentDate={setCurrentDate}
+            />
+          ))}
         </View>
       )}
     </View>
